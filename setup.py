c1c6d37947ce889ca6f9f111a4293a8f9fea1d33
Nicolas Garcia Sastre <n.garcia.sastre@gmail.com>
2024-01-03 18:40:51 +0100

    fixed setup

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup
 
 setup(
     name='MetroValencia',
-    version='1.0',
+    version='1.0.2',
     scripts=['./metrovalencia/metrovalencia_cli.py'],
     author='NikoConn',
     description='Api for MetroValencia.',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
     packages=['metrovalencia'],
     install_requires=[
         'datetime',
